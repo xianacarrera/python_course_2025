ca6759fa36ad8010fb850b40a9cfa19aba64a3bb
xianacarrera <xiana.carrera.alonso@gmail.com>
2025-01-26 23:03:56 +0100

    Avances clase 3

--- pruebas.py
+++ pruebas.py
@@ -1,10 +1,1 @@
-def dividir(a, b):
-    try:
-        res = a / b
-        print(res)
-    except:
-        print("Error. b no puede ser 0")
-
-dividir(3, 0)
-
-dividir(3, 1)
+open("my_archivo2.txt", "xb")
